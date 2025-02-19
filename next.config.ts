106453afda6666ec0abeb6b73fcb40e06ce58ecd
Marcos Porto <mosther_marcos007@hotmail.com>
2025-02-19 15:57:01 -0300

    feat: add initial restaurant and menu page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "u9a6wmr3as.ufs.sh",
+      },
+      {
+        hostname: "scontent.fvdc4-1.fna.fbcdn.net",
+      },
+      {
+        hostname: "img.freepik.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
