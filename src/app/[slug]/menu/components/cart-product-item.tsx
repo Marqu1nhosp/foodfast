8e6849193c143dc511508823b82fd5cd17672bc7
Marcos Porto <mosther_marcos007@hotmail.com>
2025-02-22 07:51:32 -0300

    feat: ensume cart product quantity can be decrause

--- src/app/[slug]/menu/components/cart-product-item.tsx
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -1,16 +1,18 @@
 import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
+import { useContext } from "react";
 
 import { FormatCurrency } from "@/app/utils/format-currency";
 import { Button } from "@/components/ui/button";
 
-import { CartProduct } from "../contexts/card";
+import { CartContext, CartProduct } from "../contexts/card";
 
 interface CartItemProps {
     product: CartProduct;
 }
 
 const CartProductItem = ({ product }: CartItemProps) => {
+    const { decreaseProductQuantity } = useContext(CartContext)
     return (
         <div className="flex items-center justify-between" >
             <div className="flex items-center gap-3">
@@ -21,7 +23,7 @@ const CartProductItem = ({ product }: CartItemProps) => {
                     <p className="text-xs max-w-[90%] truncate text-ellipsis">{product.name}</p>
                     <p className="text-sm font-semibold text-left">{FormatCurrency(product.price)}</p>
                     <div className="flex items-center gap-1 text-center">
-                        <Button className="w-7 h-7 rounded-lg" variant="outline">
+                        <Button onClick={() => decreaseProductQuantity(product.id)} className="w-7 h-7 rounded-lg" variant="outline">
                             <ChevronLeftIcon />
                         </Button>
                         <p className="w-7 text-xs">{product.quantity}</p>
